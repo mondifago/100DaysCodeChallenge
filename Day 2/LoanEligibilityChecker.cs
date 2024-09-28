0bf3865af8153a05eb44f05f2d1e2cf69c3e6881
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-28 22:41:41 +1000

    IsEligibleForLoan method created

---
+++ Day 2/LoanEligibilityChecker.cs
@@ -0,0 +1,10 @@
+﻿namespace _100DaysCodeChallenge.Day_2
+{
+    public class LoanEligibilityChecker
+    {
+        public bool IsEligibleForLoan(Applicant applicant)
+        {
+            return applicant.Age >= 21 && applicant.MonthlyIncome >= 2000 && applicant.YearsEmployed >= 2;
+        }
+    }
+}
