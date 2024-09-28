6f90ba57745ee0ff5180ae786123f55cd850980b
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-28 22:13:34 +1000

    Applicant properties declared

--- Day 2/Applicant.cs
+++ Day 2/Applicant.cs
@@ -2,5 +2,9 @@
 {
     public class Applicant
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double MonthlyIncome { get; set; }
+        public int YearsEmployed { get; set; }
     }
 }
