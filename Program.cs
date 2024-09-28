7064d0b0ef30351ffe3f37d13a8c15978d10f9c4
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-28 20:14:04 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _100DaysCodeChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
